a8f819505e24354f7a394485c890dd41a694455a
Adam Skoczeń <adamsk91@wp.pl>
2022-11-21 17:28:04 +0100

    Task 2 complete

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const liClass = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+const createLi = document.createElement("li");
+createLi.textContent = ingredient;
+createLi.classList.add('list');
+liClass.append(createLi);
+});
