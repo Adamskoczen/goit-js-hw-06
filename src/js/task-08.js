9d26e333895c66181021e98a9b1c78e663683a9a
Adam Skoczeń <adamsk91@wp.pl>
2022-11-24 20:27:37 +0100

    Task 8 to do

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const formLogin = document.querySelector('.login-form');
 
+formLogin.addEventListener('submit', e => {
+    e.preventDefault();
+    const {
+        elements: { email, password }
+    } = e.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert('Uzupełnij formularz!');
+    };
+    formData.changeMail = email.value;
+    formData.changePassword = password.value;
+    console.log(formData.email);
+    console.log(formData.password);
+    formLogin.reset();
+});
+
+const formData = {
+    email,
+    password,
+    changeMail(newMail) {
+        this.email = changeEmail;
+    },
+    changePassword(newPassword) {
+        this.password = this.changePassword;
+    },
+};
