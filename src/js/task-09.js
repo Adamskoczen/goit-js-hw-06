e91a6b7cc891d00fd4c31ab86642edb13599986b
Adam Skoczeń <adamsk91@wp.pl>
2022-11-26 07:49:34 +0100

    Task 9 complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const hexText = document.querySelector('.color');
+const hexBtn = document.querySelector('.change-color');
+
+hexBtn.addEventListener('click', e => {
+  hexText.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
