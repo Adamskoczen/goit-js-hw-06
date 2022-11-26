a9e5fc7609c153ee06f95edfab3addc6282c13be
Adam Skoczeń <adamsk91@wp.pl>
2022-11-26 08:29:35 +0100

    Task 10 bonus complete

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputIn = document.querySelector('input');
+const createElements = document.querySelector('[data-create]');
+const destroyElements = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+let divCounter = 30;
+
+const createBoxes = amount => {
+  console.log(amount)
+    for (let i = 0; i < amount ; i++) {
+    const divNew = document.createElement('div');
+    divNew.style.backgroundColor = getRandomHexColor();
+    divNew.style.height = `${divCounter}px`;
+    divNew.style.width = `${divCounter}px`;
+    divCounter += 10;
+    boxes.appendChild(divNew);
+  }
+};
+
+const destroyBoxes = () => {
+  divCounter = 30;
+  inputIn.value = "";
+  boxes.innerHTML = "";
+  };
+
+createElements.addEventListener('click', () => {
+  createBoxes(inputIn.value);
+});
+destroyElements.addEventListener('click', destroyBoxes);
